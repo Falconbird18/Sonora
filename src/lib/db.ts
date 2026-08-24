a61e034f6d0f7db602cffcd7aa50ee309509d109
Austin Tallent <158117022+Falconbird18@users.noreply.github.com>
2026-08-23 23:32:04 -0400

    feat: persist synced library folders

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,21 @@
 import Dexie, { type Table } from 'dexie';
-import type { ScoreItem, AnnotationRecord } from './types';
+import type { ScoreItem, AnnotationRecord, FolderSource } from './types';
 
 export class MusicDatabase extends Dexie {
 	scores!: Table<ScoreItem, string>;
 	annotations!: Table<AnnotationRecord, string>;
+	folders!: Table<FolderSource, string>;
 
 	constructor() {
 		super('IMSPL_MusicViewer_DB');
 		this.version(1).stores({
 			scores: 'id, title, composer, addedAt',
 			annotations: 'id, scoreId, pageNum'
+		});
+		this.version(2).stores({
+			scores: 'id, title, composer, addedAt, sourceFolderId, sourcePath',
+			annotations: 'id, scoreId, pageNum',
+			folders: 'id, name, addedAt'
 		});
 	}
 }
